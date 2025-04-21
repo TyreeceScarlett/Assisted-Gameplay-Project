58ecaa2ef8ec56fe4f196131ba93c10ffd87b3b8
Tyreece Scarlett <93916317+TyreeceScarlett@users.noreply.github.com>
2025-04-21 17:20:25 +0100

    moving forwards

--- Assist Gaming Test/Assets/Scripts/MovementStateManager.cs
+++ Assist Gaming Test/Assets/Scripts/MovementStateManager.cs
@@ -14,6 +14,9 @@ public class MovementStateManager : MonoBehaviour
     [SerializeField] LayerMask groundMask;
     Vector3 spherePos;
 
+    [SerializeField] float gravity = -9.81f;
+    Vector3 velocity;
+
 
     void Start()
     {
@@ -23,6 +26,7 @@ public class MovementStateManager : MonoBehaviour
     void Update()
     {
         GetDirectionAndMove();
+        Gravity();
     }
 
     void GetDirectionAndMove()
@@ -40,5 +44,19 @@ public class MovementStateManager : MonoBehaviour
         spherePos = new Vector3(transform.position.y - groundYOffset, transform.position.z);
         if (Physics.CheckSphere(spherePos, controller.radius - 0.05f, groundMask)) return true;
         return false;
+    }
+
+    void Gravity()
+    {
+        if (!IsGrounded()) velocity.y += gravity * Time.deltaTime;
+        else if (velocity.y < 0) velocity.y = -2;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(spherePos, controller.radius - 0.05f);
     }
 }
