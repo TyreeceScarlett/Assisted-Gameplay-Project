50e62135b379130969f646a039d3293843975e79
Tyreece Scarlett <93916317+TyreeceScarlett@users.noreply.github.com>
2025-04-22 17:12:24 +0100

    placeholder stuff

--- Assist Gaming Test/Assets/Scripts/MovementStateManager.cs
+++ Assist Gaming Test/Assets/Scripts/MovementStateManager.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Windows; // optional, may not be needed
 
 public class MovementStateManager : MonoBehaviour
 {
     public float moveSpeed = 3;
     [HideInInspector] public Vector3 dir;
+
     float hzInput, vInput;
     CharacterController controller;
 
@@ -17,46 +17,52 @@ public class MovementStateManager : MonoBehaviour
     [SerializeField] float gravity = -9.81f;
     Vector3 velocity;
 
-
     void Start()
     {
-        controller = GetComponent<CharacterController>(); // <--- This is the missing part
+        controller = GetComponent<CharacterController>();
     }
 
     void Update()
     {
         GetDirectionAndMove();
-        Gravity();
+        ApplyGravity();
     }
 
     void GetDirectionAndMove()
     {
-        hzInput = UnityEngine.Input.GetAxis("Horizontal");
-        vInput = UnityEngine.Input.GetAxis("Vertical");
+        hzInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
 
         dir = transform.forward * vInput + transform.right * hzInput;
-
         controller.Move(dir * moveSpeed * Time.deltaTime);
     }
 
     bool IsGrounded()
     {
-        spherePos = new Vector3(transform.position.y - groundYOffset, transform.position.z);
-        if (Physics.CheckSphere(spherePos, controller.radius - 0.05f, groundMask)) return true;
-        return false;
+        // Corrected spherePos calculation
+        spherePos = new Vector3(transform.position.x, transform.position.y - groundYOffset, transform.position.z);
+        return Physics.CheckSphere(spherePos, controller.radius - 0.05f, groundMask);
     }
 
-    void Gravity()
+    void ApplyGravity()
     {
-        if (!IsGrounded()) velocity.y += gravity * Time.deltaTime;
-        else if (velocity.y < 0) velocity.y = -2;
+        if (!IsGrounded())
+            velocity.y += gravity * Time.deltaTime;
+        else if (velocity.y < 0)
+            velocity.y = -2;
 
         controller.Move(velocity * Time.deltaTime);
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(spherePos, controller.radius - 0.05f);
+        if (controller == null)
+            controller = GetComponent<CharacterController>();
+
+        if (controller != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(spherePos, controller.radius - 0.05f);
+        }
     }
 }
