38ae43c4f8154886d31cfeb8a39465d1f83bbc15
Tyreece Scarlett <93916317+TyreeceScarlett@users.noreply.github.com>
2025-05-15 22:38:09 +0100

    Ignore this push

--- Anime Shooter Tester/Assets/Scripts/Weapons/WeaponClassManager.cs
+++ Anime Shooter Tester/Assets/Scripts/Weapons/WeaponClassManager.cs
@@ -5,108 +5,14 @@ using UnityEngine.Animations.Rigging;
 
 public class WeaponClassManager : MonoBehaviour
 {
-    [SerializeField] TwoBoneIKConstraint leftandIK;
+    [SerializeField] TwoBoneIKConstraint leftHandIK;
     public Transform recoilFollowPos;
-    public WeaponManager[] weapons;
-    int currentWeaponIndex;
-
     ActionStateManager actions;
-
-    private void Awake()
-    {
-        actions = GetComponent<ActionStateManager>();
-        currentWeaponIndex = 0;
-
-        for (int i = 0; i < weapons.Length; i++)
-        {
-            if (weapons[i] != null)
-            {
-                weapons[i].gameObject.SetActive(i == 0);
-            }
-            else
-            {
-                Debug.LogWarning($"Weapon slot {i} is null in WeaponClassManager!");
-            }
-        }
-
-        if (weapons.Length > 0 && weapons[0] != null)
-            SetCurrentWeapon(weapons[0]);
-    }
 
     public void SetCurrentWeapon(WeaponManager weapon)
     {
-        if (weapon == null)
-        {
-            Debug.LogError("WeaponManager is null when setting current weapon!");
-            return;
-        }
-
-        if (actions == null)
-            actions = GetComponent<ActionStateManager>();
-
-        StartCoroutine(SwapWeaponCoroutine(weapon));
-    }
-
-    private IEnumerator SwapWeaponCoroutine(WeaponManager weapon)
-    {
-        if (leftandIK != null)
-            leftandIK.weight = 0f;
-
-        if (actions != null)
-            actions.SetWeapon(weapon);
-
-        yield return new WaitForSeconds(0.5f);
-
-        if (leftandIK != null)
-        {
-            leftandIK.data.target = weapon.leftHandTarget;
-            leftandIK.data.hint = weapon.leftHandHint;
-            leftandIK.weight = 1f;
-        }
-    }
-
-    public void ChangeWeapon(float direction)
-    {
-        if (weapons.Length == 0)
-        {
-            Debug.LogWarning("No weapons assigned in WeaponClassManager!");
-            return;
-        }
-
-        if (weapons[currentWeaponIndex] != null)
-            weapons[currentWeaponIndex].gameObject.SetActive(false);
-
-        if (direction < 0)
-        {
-            currentWeaponIndex = (currentWeaponIndex == 0) ? weapons.Length - 1 : currentWeaponIndex - 1;
-        }
-        else
-        {
-            currentWeaponIndex = (currentWeaponIndex == weapons.Length - 1) ? 0 : currentWeaponIndex + 1;
-        }
-
-        if (weapons[currentWeaponIndex] != null)
-        {
-            weapons[currentWeaponIndex].gameObject.SetActive(true);
-            SetCurrentWeapon(weapons[currentWeaponIndex]);
-            Debug.Log($"Switched to weapon: {weapons[currentWeaponIndex].name}");
-        }
-        else
-        {
-            Debug.LogWarning($"Weapon at index {currentWeaponIndex} is null!");
-        }
-    }
-
-    public void WeaponPutAway(float scrollDirection)
-    {
-        ChangeWeapon(scrollDirection);
-    }
-
-    public void WeaponPullOut()
-    {
-        if (actions != null)
-        {
-            actions.SwitchState(actions.Default);
-        }
+        if (actions == null) actions = GetComponent<ActionStateManager>();
+        leftHandIK.data.target = weapon.leftHandTarget;
+        leftHandIK.data.hint = weapon.leftHandHint;
     }
 }
